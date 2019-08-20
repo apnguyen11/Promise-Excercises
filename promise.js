4ab770417a44b6a8f7e96aaa715f59558bddb2ac
Andy Nguyen <apnguyen11@yahoo.com>
2019-08-19 20:41:15 -0500

    learned more about promises implementing .then and .catch to resolve and reject errors

--- promise.js
+++ promise.js
@@ -19,15 +19,59 @@ const promises = urls.map(buildPromise)
 promises[0]
   .then(promises[1])
   .then(function(){
-      console.log("2 promises")
+      console.log("2 promises fetched")
   })
 
 Promise.all(promises)
   .then(function(promise){
-        console.log("they have all resolved")
+        console.log("all data was fecthed")
         console.log(promise)
   })
 
+ 
+ 
+  function addNumbers(x, y){
+      return new Promise((resolve, reject) => {
+          if(typeof x == 'number' && typeof y == 'number'){
+              resolve(x + y)
+          } else {
+              reject('These are not numbers!')
+          }
+      })
+  }
+
+  addNumbers(3, "d").then((message) => {
+      console.log(message)
+  }).catch((error) => {
+      console.log(error)  
+  })
+
+  let dropButteredPromisified = () => {
+    let drop = Math.random();
+    console.log(drop)
+    new Promise((resolve, reject) => {
+    if (drop < 0.5) {
+       resolve(console.log("Landed butter-side up!"));
+    } else if (drop > 0.5) {
+       reject(console.log("Landed butter-side down..."));
+    } else {
+        null
+    }
+  })
+ }
+
+ dropButteredPromisified() //.then(()=>{
+//     alert('Whew, that was close!');
+//  }).catch(()=>{
+//     alert('Well shucks, there goes my toast...');
+//  });
+
+    // return Promise(
+        //     function(x, y){
+        //         return x + y
+        //     }
+        // )
+  
 
 
 //     function pickURL(eachURL){
